a8ee4f9246de7f2ef3af9c65310a35bdf91ff758
RaJami1205 <rauljamiralfarorodriguez512@gmail.com>
2025-03-13 19:39:39 -0600

    Inicialización del Proyecto

---
+++ main.c
@@ -0,0 +1,39 @@
+/*
+ * Archivo Principal: main.c
+ *
+ * Proyecto 1 de Lenguajes de Programación 
+ *      
+ * Autores:
+ *      Raúl Alfaro
+ *      Eyden Días
+ */
+#include <stdio.h>
+
+int main() {
+    int opcion;
+
+    printf("==================================\n");
+    printf("Bienvenido a Supermercados Walmart\n");
+    printf("==================================\n");
+    printf("Menu de Opciones\n");
+    printf("1. Administracion\n");
+    printf("2. Generales\n");
+
+    printf("\n");
+    printf("Ingrese la opcion: ");
+    scanf("%d", &opcion);
+
+    switch(opcion) {
+        case 1:
+            printf("Has seleccionado Administracion.\n");
+            break;
+        case 2:
+            printf("Has seleccionado Generales.\n");
+            break;
+        default:
+            printf("Opcion invalida. Por favor, ingrese 1 o 2.\n");
+            break;
+    }
+
+    return 0;
+}
