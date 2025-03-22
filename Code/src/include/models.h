c5b42e735047fb18096d95382c7d7b3ba000a4f0
Eyden Su <eyden.su.diaz22@gmail.com>
2025-03-22 14:30:00 -0600

    archivos de encabezado

---
+++ Code/src/include/models.h
@@ -0,0 +1,69 @@
+#ifndef MODELS_H
+#define MODELS_H
+
+#define MAX_LINES 100  
+#define MAX_LENGTH 100 
+
+// Estructura para el usuario administrador
+typedef struct {
+    char username[50];
+    char password[100];
+} User;
+
+// Estructura para el punto de venta
+typedef struct {
+    char name[100];
+    char legal_entity_id[100];
+    char phone_number[50];
+} Sales_Point;
+
+// Estructura para la familia de productos
+typedef struct {
+    char name[MAX_LENGTH];
+} Product_Family;
+
+// Estructura para los productos
+typedef struct {
+    char name[100];
+    char cost[10];
+    char price[10];
+    int stock;
+    int product_family_id;
+} Product;
+
+// Estructura para las cotizaciones
+typedef struct {
+    char sub_total[10];
+    char total_taxes[10];
+    char total[10];
+    char state[100];
+} Quotation;
+
+// Estructura para los las líneas de cotizaciones
+typedef struct {
+    int quotation_id;
+    int product_id;
+    int quantity;
+    char line_sub_total[10];
+    char line_total_taxes[10];
+} Quotation_Line;
+
+// Estructura para las facturas
+typedef struct {
+    int quotation_reference_id;
+    char date[10];
+    char sub_total[10];
+    char total_taxes[10];
+    char total[10];
+} Invoice;
+
+// Estructura para las lineas de facturas
+typedef struct {
+    int invoice_id;
+    int product_id;
+    int quantity;
+    char line_sub_total[10];
+    char line_total_taxes[10];
+} Invoice_Line;
+
+#endif /* MODELS_H */
---
+++ Code/src/include/products.h
@@ -0,0 +1,10 @@
+#ifndef PRODUCTS_H
+#define PRODUCTS_H
+
+#include "models.h"
+#include "db_handler.h"
+
+void setProductFamily();
+void setProduct();
+
+#endif /* PRODUCTS_H */
